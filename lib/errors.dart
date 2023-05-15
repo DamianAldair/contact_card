f9eca1d25624902bff180574a598a4d86762c54d
Damian Aldair <daldairgp@gmail.com>
2023-05-15 18:32:31 -0400

    Errors documentation added

--- lib/errors.dart
+++ lib/errors.dart
@@ -1,12 +1,18 @@
+/// Error that occurs when there are problems parsing
+/// `MeCard` or `VCard` to plain text and vice versa.
 class CardParsingError extends Error {
   final Object? message;
   CardParsingError([this.message]);
 }
 
+/// Error that occurs when there are problems parsing
+/// `MeCard` to plain text and vice versa.
 class MeCardParsingError extends CardParsingError {
   MeCardParsingError([super.message]);
 }
 
+/// Error that occurs when there are problems parsing
+/// `VCard` to plain text and vice versa.
 class VCardParsingError extends CardParsingError {
   VCardParsingError([super.message]);
 }
